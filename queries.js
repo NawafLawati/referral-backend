543e55f831daf261bd2c1157001f385bcba88f01
Nawaf Al Lawati <46934129+NawafLawati@users.noreply.github.com>
2024-04-18 12:30:30 +0400

    complete backend

---
+++ queries.js
@@ -0,0 +1,85 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'me',
+  host: 'localhost',
+  database: 'api',
+  password: 'password',
+  port: 5432,
+})
+const getUsers = (request, response) => {
+  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const getUserById = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const getUserByCode = (request, response) => {
+    const code = request.params.code
+
+    pool.query('SELECT * FROM users WHERE code = $1', [code], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+
+const createUser = (request, response) => {
+  const { name, email, code } = request.body
+
+  pool.query('INSERT INTO users (name, email, code) VALUES ($1, $2, $3)', [name, email, code], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send(`User added with ID: ${results.insertId}`)
+  })
+}
+
+const updateUser = (request, response) => {
+  const id = parseInt(request.params.id)
+  const { name, email, code } = request.body
+
+  pool.query(
+    'UPDATE users SET name = $1, email = $2, code = $3 WHERE id = $4',
+    [name, email, code, id],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`User modified with ID: ${id}`)
+    }
+  )
+}
+
+const deleteUser = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(`User deleted with ID: ${id}`)
+  })
+}
+
+module.exports = {
+  getUsers,
+  getUserById,
+  getUserByCode,
+  createUser,
+  updateUser,
+  deleteUser,
+}
